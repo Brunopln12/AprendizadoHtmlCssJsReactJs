7abb99d2c6831c1a64f5086940ebd6aa772db972
Brunopln12 <bruno_pinho_lobo@hotmail.com>
2022-06-29 17:20:31 -0300

    Exercício da aula 16

---
+++ Curso em vídeo/JavaScript/Aula 16 - Exercícios/Modelo/script.js
@@ -0,0 +1,39 @@
+var resultado = document.querySelector('#result')
+var elemento = document.getElementById('elemento')
+var tab = document.getElementById('seltab')
+var array = []
+
+function adicionar(){
+    if(elemento.value.length == 0){
+       return window.alert('Por favor, digite um número!')
+    } else if (elemento.value < 1 || elemento.value > 100){
+        return window.alert('O valor digitado deve ser entre 1 e 100')
+    } else if(array.includes(Number(elemento.value))){
+        return window.alert('Já contém este elemento!')
+    }
+    else{
+        var numero = Number(elemento.value)
+        var item = document.createElement('option')
+        tab.appendChild(item)
+        array.push(Number(elemento.value))
+    }
+    return item.text = `Vetor ${numero} adicionado`
+}
+
+function analisar(){
+    if(elemento.value.length == 0){
+        return window.alert('Por favor, digite um número!')
+    } else if (elemento.value < 1 || elemento.value > 100){
+         return window.alert('O valor digitado deve ser entre 1 e 100')
+    } else{
+       // função para ordenar um array numérico
+        var arrayOrdenado = array.sort((a,b)=> a-b)
+        var arrayMaior = array[array.length-1]
+        var arrayMenor = array[0]
+        // Soma todos os valores internos do array
+        var arraySoma = array.reduce((acc, cur)=> acc + cur)
+        var arrayMedia = arraySoma / array.length
+    }    
+    return resultado.innerHTML = `Ao todo temos ${array.length}. números cadastrados <br> O maior valor do vetor é ${arrayMaior}. <br> O menor valor do vetor é ${arrayMenor}. <br> A soma dos valores internos são ${arraySoma}. <br> A média dos valores digitados é ${arrayMedia}.`
+}
+
