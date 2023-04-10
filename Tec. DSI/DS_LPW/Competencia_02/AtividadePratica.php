a78551ec45c3dd8816f3c25fab3ab933305acf71
Bruno pinho Lôbo Nascimento <bruno_pinho_lobo@hotmail.com>
2023-04-10 23:14:29 +0000

    Primeiros passos em PHP

---
+++ Tec. DSI/DS_LPW/Competencia_02/AtividadePratica.php
@@ -0,0 +1,9 @@
+<?php
+    // Fórmula para calcular o volume de um cone.
+    $raio = 3;
+    $altura = 6;
+    $pi = 3.14159;
+    $volume = ($pi*($raio**2)*$altura)/3;
+
+    echo "O Volume do cone é: $volume";
+?>
