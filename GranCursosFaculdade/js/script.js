be4ea0d77e2bc4fe5304a0062c6cb8b04798eb72
brunopln12 <bruno_pinho_lobo@hotmail.com>
2024-03-21 19:16:32 -0300

    Aula de nivelamento de conteúdo

---
+++ GranCursosFaculdade/js/script.js
@@ -0,0 +1,27 @@
+let tabuleiro;
+let board;
+let aviso;
+let jogador;
+let linha;
+let coluna;
+
+function iniciar() {
+    tabuleiro =[];
+    board = document.getElementById("board");
+    aviso = document.getElementById("aviso");
+    jogador = 1
+
+    console.log(tabuleiro);
+}
+
+function exibir() {
+    
+}
+
+function jogar() {
+    
+}
+
+function checar() {
+    
+}
